6b59b966d751a1073a10f5b7725fffed48adc2f2
alescuma <113590784+alescuma@users.noreply.github.com>
2022-10-14 20:07:17 +0200

    Create Seznamukol1.py

---
+++ Želví grafika/Seznamukol1.py
@@ -0,0 +1,10 @@
+dol = int(input("dol hranice "))
+hor = int(input("hor hranice: "))
+1
+for num in range(dol,hor+1):
+    if num > 1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
