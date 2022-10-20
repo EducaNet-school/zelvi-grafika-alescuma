3e07837610bb8f9aaf96f45b79b71616fb41f1e6
alescuma <113590784+alescuma@users.noreply.github.com>
2022-10-20 11:50:00 +0200

    Create FibonaccihoRadaUkol2.py
    dodělat

---
+++ Seznamy1/FibonaccihoRadaUkol2.py
@@ -0,0 +1,25 @@
+
+fcisla = int(input("Kolik chcete?"))
+
+c1, c2 = 0, 1
+vypoc = 0
+
+
+if fcisla <= 0:
+   print("POUZE KLADNÉ")
+
+elif fcisla == 1:
+   print("Fibonacci řada max",fcisla,":")
+   print(c1)
+
+else:
+   print("Fibonacci řada:")
+   while vypoc < fcisla:
+       print(c1)
+       nth = c1 + c2
+
+       c1 = c2
+       c2 = nth
+       vypoc += 1
+
+ #Dodělatz list až dojdu dom
